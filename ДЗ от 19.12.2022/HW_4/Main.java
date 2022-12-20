c4b449956cbe0ccab52f2f043c87dc754e5e18a4
MrBizzon <info.proect@mail.ru>
2022-12-20 18:40:04 +0300

    Completed

--- ДЗ от 19.12.2022/HW_4/Main.java
+++ ДЗ от 19.12.2022/HW_4/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
     FamilyMember branch = new FamilyMember();
     List<Subject> society = new ArrayList<Subject>();
-    Subject relative1 = new Subject("Иван", "Семенов", 30, Sex.Мужчина);
-    Subject relative2 = new Subject("Мария", "Васильева", 27, Sex.Женщина);
-    Subject relative3 = new Subject("Сергей", "Громов", 57, Sex.Мужчина);
-    Subject relative4 = new Subject("Нина", "Васина", 50, Sex.Женщина);
-    Subject relative5 = new Subject("Василий", "Семенов", 29, Sex.Мужчина);
-    Subject relative6 = new Subject("Семен", "Васильев", 19, Sex.Мужчина);
-    Subject relative7 = new Subject("Анна", "Брызгунова", 86, Sex.Женщина);
+    Subject relative1 = new Subject("Иван", "Зубарев", 30, Sex.Мужчина);
+    Subject relative2 = new Subject("Екатерина", "Зубарева", 27, Sex.Женщина);
+    Subject relative3 = new Subject("Семен", "Бабенко", 57, Sex.Мужчина);
+    Subject relative4 = new Subject("Светлана", "Зубарева", 50, Sex.Женщина);
+    Subject relative5 = new Subject("Василий", "Бабенко", 29, Sex.Мужчина);
+    Subject relative6 = new Subject("Иван", "Бабенко", 19, Sex.Мужчина);
+    Subject relative7 = new Subject("Раиса", "Брызгунова", 86, Sex.Женщина);
     Subject relative8 = new Subject("Олег", "Зубарев", 3, Sex.Мужчина);
 
     society.add(relative1);
@@ -26,19 +26,15 @@ public class Main {
     society.add(relative7);
     society.add(relative8);
 
-    branch.addAddiction(relative1, relative2, Interrelation.муж, Interrelation.жена);
-    branch.addAddiction(relative1, relative3, Interrelation.отец, Interrelation.отец);
-    branch.addAddiction(relative1, relative8, Interrelation.отец, Interrelation.отец);
-    branch.addAddiction(relative1, relative4, Interrelation.мать, Interrelation.отец);
-    branch.addAddiction(relative1, relative5, Interrelation.брат, Interrelation.брат);
-    branch.addAddiction(relative1, relative6, Interrelation.брат, Interrelation.брат);
+    branch.addAddiction(relative1, relative2, Interrelation.брат, Interrelation.сестра);
+    branch.addAddiction(relative1, relative3, Interrelation.правнук, Interrelation.прадед);
+    branch.addAddiction(relative1, relative4, Interrelation.внучатыйПлемянник, Interrelation.мать);
+    branch.addAddiction(relative1, relative5, Interrelation.брат, Interrelation.внучатыйДедушка);
     branch.addAddiction(relative1, relative7, Interrelation.внук, Interrelation.бабушка);
-
+    branch.addAddiction(relative1, relative8, Interrelation.сын, Interrelation.отец);
     branch.addAddiction(relative5, relative8, Interrelation.дядя, Interrelation.племянник);
     branch.addAddiction(relative6, relative8, Interrelation.дядя, Interrelation.племянник);
-
-    branch.addAddiction(relative2, relative8, Interrelation.мать, Interrelation.отец);
-
+    branch.addAddiction(relative2, relative8, Interrelation.дочь, Interrelation.отец);
     branch.addAddiction(relative8, relative3, Interrelation.внук, Interrelation.дедушка);
 
     System.out.println(society);
